ad0f82e4b9271e1d7e8693444724e4c8aeece43d
Chris-Prog <37072943+Chris-Prog@users.noreply.github.com>
2022-08-08 00:17:29 -0400

    Add files via upload
    Information found within 2 hours

---
+++ script.js
@@ -0,0 +1,22 @@
+// Example Array
+const testArray=['aabb','aaaa','bbab']
+
+
+
+//Function LonestSubString
+//The function returns an object with the letter and length of the longest substring
+function longestSubString(array) {
+    return array
+//A function that will produce different combinations based on array.length
+// For Example testArray.Length = 3. 3x3=9 possible combinations
+// 
+
+//A function that will pick the best combination based on array.length
+// .join() the array with the best sequence
+// Write a condition that will keep count of the highest substring and text 
+
+//Convert the final answer into an object that will print out the lonestSubstring and Number
+
+//A return statement
+
+} console.log(longestSubString(testArray.length))
